247a00fec03ea926f850f1bfe5cf216dc70fdc8c
Nebiyou-x <nebiyoutad@gmail.com>
2025-06-19 18:33:33 +0300

    add

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world bro")
+
+	godotenv.Load(".env")
+	portstring := os.Getenv("PORT")
+
+	if portstring == "" {
+		log.Fatal("Error occured")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portstring,
+	}
+
+	log.Printf("server starting on port %v", portstring)
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
